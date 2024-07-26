72220f4b07542372daeea9b7f5e8b345d78322f6
huongluong23 <21020337@vnu.edu.vn>
2024-07-26 23:21:54 +0700

    complete ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.f0ris.sweetalert:library:1.6.2")
+    implementation ("com.google.android.material:material:1.4.0")
 }
